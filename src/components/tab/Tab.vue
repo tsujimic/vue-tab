5621bbb0318692c5882e09b61f3f96dea34379e4
takashi.tsuji <takashi.tsuji@icloud.com>
2024-06-13 08:41:27 +0900

    fix: add tab

---
+++ src/components/tab/Tab.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Home from './items/Home.vue'
+import Posts from './items/Posts.vue'
+import Archive from './items/Archive.vue'
+ 
+const currentTab = ref(0)
+
+const tabs = [
+  { name: 'Home', component: Home } ,
+  { name: 'Posts', component: Posts } ,
+  { name: 'Archive', component: Archive } ,
+]
+
+</script>
+
+<template>
+    <header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl py-2 px-3 sm:px-6 lg:px-8">
+          <h1 class="text-2xl font-bold tracking-tight text-gray-900">Tab</h1>
+        </div>
+    </header>
+    <main>
+        <div class="demo">
+            <button
+                v-for="(tab, index) in tabs"
+                :key="index"
+                :class="['tab-button', { active: currentTab === index }]"
+                @click="currentTab = index"
+            >
+            {{ tab.name }}
+            </button>
+            <KeepAlive>
+                <component :is="tabs[currentTab].component" class="tab"></component>
+            </KeepAlive>
+        </div>
+
+    </main>    
+</template>
+    
+<style scoped>
+.demo {
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
+
+.tab-button {
+  padding: 6px 10px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  background: #f0f0f0;
+  margin-bottom: -1px;
+  margin-right: -1px;
+  font-size: small;
+}
+.tab-button:hover {
+  background: #e0e0e0;
+}
+.tab-button.active {
+  background: #e0e0e0;
+}
+.tab {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+</style>
+    
---
+++ src/components/tab/items/Posts.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+
+</script>
+
+<template>
+    <div>
+        <p>Posts component</p>
+        <div class="flex flex-col py-5">
+            <div>
+                <span>count: {{ count }}</span>
+            </div>
+            <div class="space-x-1">
+                <button type="button" class="bg-blue-400 hover:bg-blue-300 text-white rounded w-10 h-10" @click="count++">+</button>
+                <button type="button" class="bg-orange-400 hover:bg-orange-300 text-white rounded w-10 h-10" @click="count--">-</button>
+            </div>
+        </div>
+
+    </div>
+</template>
